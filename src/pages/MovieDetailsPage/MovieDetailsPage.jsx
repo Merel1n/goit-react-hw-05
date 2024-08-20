51eb211490f26b1b72da31ffbff1e8bfaa03cc74
Inna Matviienko <ineskamit@gmail.com>
2024-08-20 10:45:34 +0200

    up

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 
 const MovieDetailsPage = () => {
   const { movieId } = useParams();
-  const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState(null);
   const [loader, setLoader] = useState(false);
   useEffect(() => {
     try {
